0ead08ab88d42f8011c73ee98f511f430fae5fca
Piotr Biliński <bilinski.piotr89@gmail.com>
2025-10-23 16:26:50 +0200

    poprawka importow w  astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig, envField} from "astro/config";
 
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
@@ -19,4 +19,12 @@ export default defineConfig({
   //   mode: "standalone",
   // }),
   adapter: cloudflare(),
+  env: {
+    schema: {
+      SUPABASE_URL: envField.string({ context: "server", access: "secret" }),
+      SUPABASE_KEY: envField.string({ context: "server", access: "secret" }),
+      OPENROUTER_API_KEY: envField.string({ context: "server", access: "secret" }),
+      AI_MODELNAME: envField.string({ context: "server", access: "public", default: "openai/gpt-4o-mini" }),
+    }
+  },
 });
