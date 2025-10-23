26ae64ee03cae5a93a0fd7427d8e29eabe08d6db
Piotr Biliński <bilinski.piotr89@gmail.com>
2025-10-23 16:29:47 +0200

    poprawka lintera

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig, envField} from "astro/config";
+import { defineConfig, envField } from "astro/config";
 
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
@@ -25,6 +25,6 @@ export default defineConfig({
       SUPABASE_KEY: envField.string({ context: "server", access: "secret" }),
       OPENROUTER_API_KEY: envField.string({ context: "server", access: "secret" }),
       AI_MODELNAME: envField.string({ context: "server", access: "public", default: "openai/gpt-4o-mini" }),
-    }
+    },
   },
 });
